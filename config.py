39553d69d3054d04393150249ffa10ce38cfb07e
Bad Boy X Ashu <160344472+BadBoyXAshu@users.noreply.github.com>
2024-04-30 03:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,19 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "29917436")
+    API_HASH  = os.environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6685420902:AAFDN-nygb-Q_uBqCPKvk4XrqNMPQCsXwEI") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","AshutoshGoswami24")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://bharathkalladi38:refaj5BccvHhG2Zl@cluster0.wvsv51o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/f9b3bd1fef4824c687d1c.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7062828064').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002114413910"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
